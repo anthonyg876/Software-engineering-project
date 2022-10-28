d427c9dbb4b3254865bc802281f2fcc103f7509e
Admin <Admin@Matts-MBP.lan>
2022-10-28 18:40:13 -0400

    Streamlit theme hidden file and more styling

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ The instructions for how to run our project are the following:
 1. Install Python
 2. Install Pip
 3. Go to command line and do pip install streamlit
-4. On the command line, go to where you have our repository stored on your computer (where Home.py is) and run "streamlit run Home.py" without quotations.
+4. On the command line, go to where you have our repository stored on your 
+computer (where Home.py is) and run "streamlit run Home.py" without 
+quotations.
--- pages/Profile.py
+++ pages/Profile.py
@@ -1,20 +1,40 @@
-from gc import callbacks
 import streamlit as st
 
 st.title("This is the Site Profile Page")
 
-col1, col2, col3, = st.columns([1, 1, 2], gap="small")
+st.markdown(f"""<div class="emptyDiv"></div>""", unsafe_allow_html=True)
 
-name = "Matthew"
+name = "Matthew Johnson"
+email = "email@gmail.com"
+county = "Some County"
+phone_number = "727-777-7777"
+zip_code = "33333"
+income = 20000
 
-with col1:
-    st.write("Name: ", name)
 
-with col2:
-    editbtn = st.button("Edit")
+# Profile data
+st.markdown(f"""<h3 class="profile-info">Name: {name}</h3>""", unsafe_allow_html=True)
+st.markdown(f"""<h3 class="profile-info">Email: {email}</h3>""", unsafe_allow_html=True)
+st.markdown(f"""<h3 class="profile-info">County: {county}</h3>""", unsafe_allow_html=True)
+st.markdown(f"""<h3 class="profile-info">Phone Number: {phone_number}</h3>""", unsafe_allow_html=True)
+st.markdown(f"""<h3 class="profile-info">Zip Code: {zip_code}</h3>""", unsafe_allow_html=True)
+st.markdown(f"""<h3 class="profile-info">Income: ${income}</h3>""", unsafe_allow_html=True)
 
-if editbtn:
-    st.radio(label="Change field", options=["Name", "Email", "Phone number"])
+
+st.markdown(f"""<div class="emptyDiv"></div>""", unsafe_allow_html=True)
+st.markdown(f"""<div class="emptyDiv"></div>""", unsafe_allow_html=True)
+
+st.markdown(f"""<h3>Edit your info</h3>""", unsafe_allow_html=True)
+
+editing_choices = st.multiselect("Choose what you'd like to change", 
+                options=["name", "email", "county", "phone number", "zip code", "income"])
+
+for i in range(len(editing_choices)):
+    changed_profile_value = st.text_input(f"Change {editing_choices[i]}", key=i)
+    st.write(changed_profile_value)
+
+
+st.write(editing_choices)
 
 
 with open("styles.css") as f:
