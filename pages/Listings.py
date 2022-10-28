525ac6a57cbb6ce9a08b61dfcf69585776428b81
Admin <Admin@Matts-MBP.lan>
2022-10-28 12:09:32 -0400

    Styling started

--- pages/Listings.py
+++ pages/Listings.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title("Listings")
+
+
+
+with open("styles.css") as f:
+    styling = f.read()
+
+st.markdown(f"""<style>{styling}</style>""", unsafe_allow_html=True)
--- pages/Profile.py
+++ pages/Profile.py
@@ -1,3 +1,23 @@
+from gc import callbacks
 import streamlit as st
 
 st.title("This is the Site Profile Page")
+
+col1, col2, col3, = st.columns([1, 1, 2], gap="small")
+
+name = "Matthew"
+
+with col1:
+    st.write("Name: ", name)
+
+with col2:
+    editbtn = st.button("Edit")
+
+if editbtn:
+    st.radio(label="Change field", options=["Name", "Email", "Phone number"])
+
+
+with open("styles.css") as f:
+    styling = f.read()
+
+st.markdown(f"""<style>{styling}</style>""", unsafe_allow_html=True)
