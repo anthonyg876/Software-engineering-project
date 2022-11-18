5bead68bf64f61aa26de41b2670c7a2b0c59c94d
mkerekes1427 <mkerekes53@gmail.com>
2022-11-18 15:14:42 -0500

    Profile buttons and page access

--- pages/Listings.py
+++ pages/Listings.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+if "user" not in st.experimental_get_query_params():
+    st.experimental_set_query_params(user="no")
+
+
 def categoryImg(category):
 
     if category == "hygienics":
@@ -17,85 +21,91 @@ def categoryImg(category):
     else:
         st.image("img/jelly-beans.png",use_column_width="always")
 
-filterOptions = st.multiselect("Select the filter category",
-                        options=["catagory", "county",
-                        "Business name"])
 
-for i in range(len(filterOptions)):
-    changed_profile_value = st.text_input(f"Change {filterOptions[i]}", key=i)
-    st.write("you wrote",changed_profile_value)
+if st.experimental_get_query_params()["user"][0] == "no":
+    st.title("Login to see this page")
 
-price = st.slider("Listings price", value = (0,100), step = 1)
-st.write(price) #debuging
+else:
 
-st.title("Listings")
+    filterOptions = st.multiselect("Select the filter category",
+                            options=["category", "county",
+                            "Business name"])
 
-listingbutton = st.button("Show listings")
+    for i in range(len(filterOptions)):
+        filter_col = st.text_input(f"Filter {filterOptions[i]}", key=i)
+        st.write("you wrote", filter_col)
 
-price1 = 20
-quatitiy = 10
-county = "pinellas"
-address = "12 buckel my shoue,Tampa,FL"
-items = 0
-phone = 1111111
-cat = "food"
+    price = st.slider("Listings price", value = (0,100), step = 1)
+    st.write(price) #debuging
 
-col1, col2, col3 = st.columns(3)
+    st.title("Listings")
 
-numItems = 19
+    listingbutton = st.button("Show listings")
 
-for i in range(numItems):
+    price1 = 20
+    quatitiy = 10
+    county = "pinellas"
+    address = "12 buckel my shoue,Tampa,FL"
+    items = 0
+    phone = 1111111
+    cat = "food"
 
-    if i % 3 == 0:
+    col1, col2, col3 = st.columns(3)
 
-        with col1:
-            st.markdown("""<p class= "itemHead">CVS</h1>
-                            <p class = "titleHead">Tooth paste</h2>""",
-                            unsafe_allow_html=True)
-            categoryImg(cat)
-            st.markdown(f"""
-                            <p class = "itemInfo">price ${price1}</h3>
-                            <p class = "itemInfo"> Quantity {quatitiy} </h3>
-                            <p class = "itemInfo">county : {county}</h3>
-                            <p class = "itemInfo">address {address}</h3>
-                            <p class = "itemInfo">phone # {phone}</h3>""",
-                            unsafe_allow_html=True)
+    numItems = 19
 
-            st.markdown("""<div class="emptyItemDiv"></div>""", unsafe_allow_html=True)
+    for i in range(numItems):
 
-    elif i % 3 == 1:
+        if i % 3 == 0:
 
-        with col2:
-            st.markdown("""<p class= "itemHead">CVS</h1>
-                            <p class = "titleHead">Tooth paste</h2>""",
-                            unsafe_allow_html=True)
-            categoryImg(cat)
-            st.markdown(f"""
-                            <p class = "itemInfo">price ${price1}</h3>
-                            <p class = "itemInfo"> Quantity {quatitiy} </h3>
-                            <p class = "itemInfo">county : {county}</h3>
-                            <p class = "itemInfo">address {address}</h3>
-                            <p class = "itemInfo">phone # {phone}</h3>""",
-                            unsafe_allow_html=True)
+            with col1:
+                st.markdown("""<p class= "itemHead">CVS</h1>
+                                <p class = "titleHead">Tooth paste</h2>""",
+                                unsafe_allow_html=True)
+                categoryImg(cat)
+                st.markdown(f"""
+                                <p class = "itemInfo">price ${price1}</h3>
+                                <p class = "itemInfo"> Quantity {quatitiy} </h3>
+                                <p class = "itemInfo">county : {county}</h3>
+                                <p class = "itemInfo">address {address}</h3>
+                                <p class = "itemInfo">phone # {phone}</h3>""",
+                                unsafe_allow_html=True)
 
-            st.markdown("""<div class="emptyItemDiv"></div>""", unsafe_allow_html=True)
+                st.markdown("""<div class="emptyItemDiv"></div>""", unsafe_allow_html=True)
 
-    else:
-        
-        with col3:
-            st.markdown("""<p class= "itemHead">CVS</h1>
-                            <p class = "titleHead">Tooth paste</h2>""",
-                            unsafe_allow_html=True)
-            categoryImg(cat)
-            st.markdown(f"""
-                            <p class = "itemInfo">price ${price1}</h3>
-                            <p class = "itemInfo"> Quantity {quatitiy} </h3>
-                            <p class = "itemInfo">county : {county}</h3>
-                            <p class = "itemInfo">address {address}</h3>
-                            <p class = "itemInfo">phone # {phone}</h3>""",
-                            unsafe_allow_html=True)
+        elif i % 3 == 1:
 
-            st.markdown("""<div class="emptyItemDiv"></div>""", unsafe_allow_html=True)
+            with col2:
+                st.markdown("""<p class= "itemHead">CVS</h1>
+                                <p class = "titleHead">Tooth paste</h2>""",
+                                unsafe_allow_html=True)
+                categoryImg(cat)
+                st.markdown(f"""
+                                <p class = "itemInfo">price ${price1}</h3>
+                                <p class = "itemInfo"> Quantity {quatitiy} </h3>
+                                <p class = "itemInfo">county : {county}</h3>
+                                <p class = "itemInfo">address {address}</h3>
+                                <p class = "itemInfo">phone # {phone}</h3>""",
+                                unsafe_allow_html=True)
+
+                st.markdown("""<div class="emptyItemDiv"></div>""", unsafe_allow_html=True)
+
+        else:
+            
+            with col3:
+                st.markdown("""<p class= "itemHead">CVS</h1>
+                                <p class = "titleHead">Tooth paste</h2>""",
+                                unsafe_allow_html=True)
+                categoryImg(cat)
+                st.markdown(f"""
+                                <p class = "itemInfo">price ${price1}</h3>
+                                <p class = "itemInfo"> Quantity {quatitiy} </h3>
+                                <p class = "itemInfo">county : {county}</h3>
+                                <p class = "itemInfo">address {address}</h3>
+                                <p class = "itemInfo">phone # {phone}</h3>""",
+                                unsafe_allow_html=True)
+
+                st.markdown("""<div class="emptyItemDiv"></div>""", unsafe_allow_html=True)
 
 with open("footer.html") as f:
     foot = f.read()
