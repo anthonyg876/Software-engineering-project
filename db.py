089727075205fc7f1366a0d46203fcce0d419320
Han Lee <37788519+hlee18lee46@users.noreply.github.com>
2022-11-21 02:25:31 -0500

    Update db.py
    Added update item functions for each non-PK attributes and debugged deleteBusiness, deleteParticipants
    
    Signed-off-by: Han Lee <37788519+hlee18lee46@users.noreply.github.com>

--- db.py
+++ db.py
@@ -12,7 +12,7 @@ dsn = f'{username}/{userpwd}@{host}:{port}/{service_name}'
 '''
 Adds user to the database.
 '''
-def addUser(email: str, firstName: str, lastName: str, income: int, password: str) -> str:
+def addParticipants(email: str, firstName: str, lastName: str, income: int, password: str) -> str:
     # Attempt connection to Oracle database.
     try:
         connection = oracledb.connect(dsn = dsn)
@@ -68,11 +68,33 @@ def deleteParticipant(email: str) -> None:
         print("Was not able to connect to database")
     cur = connection.cursor()
     try:
+        cur.execute("delete from ownsbusiness where email = :email", [email])
+        connection.commit()
+        output = "Deleted ownsbusiness from database."
+    except:
+        output = "Unable to deleted ownsbusiness from database"
+    try:
         cur.execute("delete from participants where email = :email", [email])
         connection.commit()
         output = "Deleted user from database."
     except:
         output = "Unable to deleted user from database"
+    connection.close()
+    return output
+
+def deleteOwnsBusiness(email: str) -> None:
+    try:
+        connection = oracledb.connect(dsn = dsn)
+        print("Connected to database")
+    except:
+        print("Was not able to connect to database")
+    cur = connection.cursor()
+    try:
+        cur.execute("delete from ownsbusiness where email = :email", [email])
+        connection.commit()
+        output = "Deleted ownsbusiness from database."
+    except:
+        output = "Unable to deleted ownsbusiness from database"
     connection.close()
     return output
 
@@ -83,6 +105,18 @@ def deleteBusiness(id: int) -> None:
     except:
         print("Was not able to connect to database")
     cur = connection.cursor()
+    try:
+        cur.execute("delete from items where bid = :bid", [id])
+        connection.commit()
+        output = "Deleted items from database."
+    except:
+        output = "Unable to deleted ownsbusiness from database"
+    try:
+        cur.execute("delete from ownsbusiness where bid = :bid", [id])
+        connection.commit()
+        output = "Deleted ownsbusiness from database."
+    except:
+        output = "Unable to deleted ownsbusiness from database"
     try:
         cur.execute("delete from business where id = :id", [id])
         connection.commit()
@@ -112,31 +146,6 @@ def addItem(name: str, category: str, postPrice: float, originalPrice: float, qu
         output = "Unable to add item into database"
     connection.close()
     return output
-'''
-Verifies user credentials and returns user info.
-'''
-def verifyLogin(email: str, password: str) -> user:
-    # Attempt connection to Oracle db.
-    try: 
-        connection = oracledb.connect(dsn=dsn)
-        print("Connected to database")
-    except:
-        print("Was not able to connect to database")
-    cur = connection.cursor()
-    # Check if the user even exists
-    cur.execute("select * from participants where email = :email", [email])
-    number = cur.fetchall() 
-    if (len(number) != 1):
-        print("User with email does not exist")
-        return None
-    
-    cur.execute("select * from participants where email = :email and password = :password", [email, password])
-    user = cur.fetchall()
-    if (len(user) != 1):
-        print("Password does not match, try again")
-        return None
-    print("Successfully verified user")
-    return user
 
 # @TODO Needs to be completed.
 def update_items():
@@ -357,27 +366,103 @@ def updateBusinessPhoneNumber(id: int, phone_Number: str) -> str:
         output = "Unsuccessful"
     connection.close()
 
-  
+def deleteItems(name: str, bid: int) -> None:
+    try:
+        connection = oracledb.connect(dsn = dsn)
+        print("Connected to database")
+    except:
+        print("Was not able to connect to database")
+    cur = connection.cursor()
+    try:
+        cur.execute("delete from items where name = :name and bid = :bid", [name, bid])
+        connection.commit()
+        output = "Deleted items from database."
+    except:
+        output = "Unable to delete items from database"
+    connection.close()
+    return output
+
+def updateItemsCategory(category: str, name: str, bid: int) -> str:
+    # Attempt connection to Oracle database.
+    try:
+        connection = oracledb.connect(dsn = dsn)
+        print("Connected to database")
+    except:
+        print("Was not able to connect to database")
+    cur = connection.cursor()
+    try:
+        cur.execute("update items set category = :category where name = :name and bid = :bid", [category,name ,bid ])
+        connection.commit()
+        output = "Updated business"
+    except:
+        output = "Unsuccessful"
+    connection.close()
+
+def updateItemsPostPrice(postprice: float, name: str, bid: int) -> str:
+    # Attempt connection to Oracle database.
+    try:
+        connection = oracledb.connect(dsn = dsn)
+        print("Connected to database")
+    except:
+        print("Was not able to connect to database")
+    cur = connection.cursor()
+    try:
+        cur.execute("update items set postprice = :postprice where name = :name and bid = :bid", [postprice,name ,bid ])
+        connection.commit()
+        output = "Updated business"
+    except:
+        output = "Unsuccessful"
+    connection.close()
+def updateItemsOriginalPrice(originalprice: float, name: str, bid: int) -> str:
+    # Attempt connection to Oracle database.
+    try:
+        connection = oracledb.connect(dsn = dsn)
+        print("Connected to database")
+    except:
+        print("Was not able to connect to database")
+    cur = connection.cursor()
+    try:
+        cur.execute("update items set originalprice = :originalprice where name = :name and bid = :bid", [originalprice,name ,bid ])
+        connection.commit()
+        output = "Updated business"
+    except:
+        output = "Unsuccessful"
+    connection.close()  
+def updateItemsQuantity(quantity: int, name: str, bid: int) -> str:
+    # Attempt connection to Oracle database.
+    try:
+        connection = oracledb.connect(dsn = dsn)
+        print("Connected to database")
+    except:
+        print("Was not able to connect to database")
+    cur = connection.cursor()
+    try:
+        cur.execute("update items set quantity = :quantity where name = :name and bid = :bid", [quantity,name ,bid ])
+        connection.commit()
+        output = "Updated business"
+    except:
+        output = "Unsuccessful"
+    connection.close()  
+    
 # def addBusiness(email: str, id: int, name: str, password: str, address: str, county: str, phoneNumber: int)
 # def addItem(name: str, category: str, postPrice: float, originalPrice: float, quantity: int, bId: int)-
 
 if __name__ == "__main__":
-    deleteParticipant("ag@gmail.com")   
-    addUser("ag@gmail.com", "Anthony", "Gravier", 85000, "boof")
-    verifyLogin("ag@gmail.com", "boof")
+    #addParticipants("ag@gamil.com", "Anthony", "Gravier", 85000, "boof")
     #deleteParticipant("realNewEmail@RealNew.com")  
-    #addUser("ag@gamil.com", "Anthony", "Gravier", 85000, "boof")
+    #deleteParticipant("ag@gamil.com")   
+    #addParticipants("ag@gamil.com", "Anthony", "Gravier", 85000, "boof")
     #updateParticipants("ag@gamil.com", "newFirstName", "newLastName",911911,"newPassword")
     #updateEmail("ag@gamil.com", "newFirstName", "newLastName",911911,"newPassword","realNewEmail@RealNew.com")
     #deleteParticipant("realNewEmail@RealNew.com")
-    #addBusiness("mk@gmail.com", 5, "Business", "pass1", "address1", "33612", 123456789)
+    #addBusiness("ag@gamil.com", 5, "Business", "pass1", "address1", "33612", 123456789)
     #updateBusiness(1, "NEwBusiness", "NEwpass1", "Newaddress1", "33615", 00000000)
     #print(deleteBusiness(5))
     #print(updateParticipantsFirstName("mk@gmail.com","RealNewNEWMatthew"))
     #print(updateParticipantsLastName("mk@gmail.com","REalNewKerekes"))
     #print(updateParticipantsIncome("mk@gmail.com",1000000))
     #print(updateParticipantsPassword("mk@gmail.com","NewPass"))
-    # addUser("ag@gamil.com", "Anthony", "Gravier", 85000, "boof")\
+    # addParticipants("ag@gamil.com", "Anthony", "Gravier", 85000, "boof")\
     #print(updateBusinessName(5, "REalNewBusiness"))
     #print(addBusiness(2, "Walmart", "password1", "100 Gainesville Road", "Sarasota County", 7271111111))
     #print(addItem("t-shirt", "clothing", 1.00, 10.00, 10, 2))
@@ -390,13 +475,18 @@ if __name__ == "__main__":
     #updateParticipantsLastName("hantetst@gmail.com","newLast")
     #updateParticipantsIncome("hantetst@gmail.com",5555)
     #updateParticipantsPassword("hantetst@gmail.com","newPAss")
-    #addUser("han2@han.com", "han","lee",5555,"pass")
+    #addParticipants("han2@han.com", "han","lee",5555,"pass")
     #addBusiness("han2@han.com", 78, "hanNewBusiness", "HanNewPass", "myNewYard", "myCNewounty", 333444)
-
-
+    #deleteBusiness(5)
+    #addItem("tampon", "misc", 33, 50, 10, 5)
+    #addItem("cigar", "misc", 10, 20, 10, 5)
+    #deleteItems("tampon", 5) 
+    #print(deleteBusiness(5))
     # Testing out selecting items
-
-
+    #updateItemsCategory("NewNew", "tampon", 5)
+    #updateItemsPostPrice(999, "tampon", 5)
+    #updateItemsOriginalPrice(9999, "tampon", 5)
+    #updateItemsQuantity(999, "tampon", 5)
     print("Participants")
     print()
 
